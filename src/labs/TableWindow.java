f2b3a006ae6de5b8e5632b6056e73ab1b9cd77a2
Romzeus <bogden08@mail.ru>
2021-06-30 19:04:17 +0300

    Updated main window's look

--- src/labs/TableWindow.java
+++ src/labs/TableWindow.java
@@ -2,7 +2,11 @@ package labs;
 
 import javax.swing.*;
 
-public class TableWindow {
+public class TableWindow extends JFrame{
     private JTable table1;
     private JPanel panel1;
+    private JButton perform_actionButton;
+    private JButton switch_languageButton;
+    private JButton exitButton;
+    private JComboBox comboBox1;
 }
